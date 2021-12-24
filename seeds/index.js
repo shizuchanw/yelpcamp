768add83aa0e89c8017ba33c0606f26d4eb60655
Flora Wang <florawang0611@gmail.com>
2021-12-24 01:20:18 -0800

    debug

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
-const Campground = require('../models/Campground');
-const { populate } = require('../models/Campground');
+const Campground = require('../models/campground');
+const { populate } = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
