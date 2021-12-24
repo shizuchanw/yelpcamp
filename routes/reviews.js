2dcb81a55646f04ecdb88c4d5102522f5a38c4bd
Flora Wang <florawang0611@gmail.com>
2021-12-24 00:59:08 -0800

    debug3

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/Campground');
+const Campground = require('../models/campground');
 const Review = require('../models/review');
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware')
 const reviews = require('../controllers/reviews')
