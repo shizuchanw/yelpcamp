298de1dd175970811b3d88ec3f0787f870edf775
Flora Wang <florawang0611@gmail.com>
2021-12-24 00:57:25 -0800

    debug

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,4 @@
-const Campground = require('../models/Campground');
+const Campground = require('../models/campground');
 const Review = require('../models/review');
 
 module.exports.createReview = async (req, res)=>{
