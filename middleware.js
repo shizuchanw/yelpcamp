0e3cd872182cab843e81020d1f8e92d531515504
Flora Wang <florawang0611@gmail.com>
2021-12-24 00:54:46 -0800

    debug

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const {campgroundSchema, reviewSchema} = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
-const Campground = require('./models/Campground');
+const Campground = require('./models/campground');
 const Review = require('./models/review');
 
 
